b0385ed649fee808e3eddcbc3ae64bb49071583e
leesanghung3 <leesanghung3@gmail.com>
2022-11-29 13:35:45 +0900

    Ch01 add

---
+++ Ch01/src/sub1/HelloWorld.java
@@ -0,0 +1,19 @@
+package sub1;
+
+public class HelloWorld {
+
+public static void main(String[] args) {
+		
+	        // 기본출력
+			System.out.println("Hello World!");
+			System.out.println("Hello Java!");
+			
+			// 한줄출력
+			System.out.print("Hello ");
+			System.out.print("Korea!");
+			
+			// 서식출력
+			System.out.println();
+			System.out.printf("%s %s", "Hello", "Busan!");
+	}	
+}
